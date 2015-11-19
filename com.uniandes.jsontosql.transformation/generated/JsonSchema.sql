824fa68ac1e2632da849d57be84c56f9c10c8209
jasmo2 <jasmo2.com@gmail.com>
2015-11-18 23:50:12 -0500

    refactor foreignKeys to alter table

--- com.uniandes.jsontosql.transformation/generated/JsonSchema.sql
+++ com.uniandes.jsontosql.transformation/generated/JsonSchema.sql
@@ -2,23 +2,27 @@
 CREATE DATABASE databaseTest;
 	
 	
-CREATE TABLE "puntajes" (
+CREATE TABLE puntajes (
 		_id int PRIMARY KEY,
 		type var(255) 
    ,
 		score decimal 
    ,
 		student_id int 
-		,
-		FOREIGN KEY (student_id)
-      		REFERENCES student(_id)
-      			 ON DELETE CASCADE
 	
-)
-CREATE TABLE "student" (
+);
+
+
+CREATE TABLE student (
 		_id int PRIMARY KEY,
 		nombre var(255) 
    ,
 		puntajes int 
 	
-)
+);
+
+
+ALTER TABLE puntajes
+	ADD FOREIGN KEY (student_id)
+  		REFERENCES student(_id)
+	  		 ON DELETE CASCADE;

0b223885d30fea4010a395a7fa8405663f8f8d99
jasmo2 <jasmo2.com@gmail.com>
2015-11-18 23:37:22 -0500

    foreignKey EGL

--- com.uniandes.jsontosql.transformation/generated/JsonSchema.sql
+++ com.uniandes.jsontosql.transformation/generated/JsonSchema.sql
@@ -4,14 +4,21 @@ CREATE DATABASE databaseTest;
 	
 CREATE TABLE "puntajes" (
 		_id int PRIMARY KEY,
-		type var(255),
-		score decimal,
-		student int,
+		type var(255) 
+   ,
+		score decimal 
+   ,
+		student_id int 
+		,
+		FOREIGN KEY (student_id)
+      		REFERENCES student(_id)
+      			 ON DELETE CASCADE
 	
 )
 CREATE TABLE "student" (
 		_id int PRIMARY KEY,
-		nombre var(255),
-		puntajes int,
+		nombre var(255) 
+   ,
+		puntajes int 
 	
 )

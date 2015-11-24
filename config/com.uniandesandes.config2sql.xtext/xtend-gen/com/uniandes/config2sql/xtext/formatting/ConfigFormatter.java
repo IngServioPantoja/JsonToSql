e201fcbf5e4852b6c372948dbb5342bf7cc84993
jasmo2 <jasmo2.com@gmail.com>
2015-11-24 12:42:15 -0500

    refactor mtc, still missing transformaction chain

--- config/com.uniandesandes.config2sql.xtext/xtend-gen/com/uniandes/config2sql/xtext/formatting/ConfigFormatter.java
+++ config/com.uniandesandes.config2sql.xtext/xtend-gen/com/uniandes/config2sql/xtext/formatting/ConfigFormatter.java
@@ -16,6 +16,7 @@ import org.eclipse.xtext.formatting.impl.FormattingConfig;
  */
 @SuppressWarnings("all")
 public class ConfigFormatter extends AbstractDeclarativeFormatter {
+  @Override
   protected void configureFormatting(final FormattingConfig c) {
   }
 }
